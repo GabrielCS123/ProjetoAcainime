3b3f541fc065dedc00b8c7d52e41909d55288772
Kaiquisalo <kaiqui.salomaomartins@gmail.com>
2026-02-18 09:09:32 -0400

    alterações gabriel

--- Jogos/memoria/script.js
+++ Jogos/memoria/script.js
@@ -6,6 +6,31 @@ const nome = localStorage.getItem("user");
 const anime = localStorage.getItem("anime");
 
 // ==========================
+// Mostrar na tela
+// ==========================
+
+let nome_anime = "";
+
+if (anime === "naruto") {
+  nome_anime = "Naruto";
+}
+else if (anime === "onepiece") {
+  nome_anime = "One Piece";
+}
+else if (anime === "demon") {
+  nome_anime = "Demon Slayer";
+}
+
+// Se não tiver dados, volta para o login
+if (!nome || !anime) {
+  window.location.href = "index.html";
+}
+
+// Mostra na tela
+document.getElementById("user").textContent = `Olá, ${nome} 👋`;
+document.getElementById("animeUser").textContent = `Anime escolhido: ${nome_anime}`;
+
+// ==========================
 // BIBLIOTECA DE IMAGENS
 // ==========================
 
